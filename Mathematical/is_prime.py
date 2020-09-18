3c4dd3b06057dfddbc53c366bc1e0b5be049b8c8
yashwanth guguloth <47604035+yashwanthguguloth24@users.noreply.github.com>
2020-09-18 11:59:02 +0530

    Create is_prime.py

---
+++ Mathematical/is_prime.py
@@ -0,0 +1,15 @@
+# computing from 2 to sqrt(n) is enough
+
+import math
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def isPrime(self, A):
+        if A<=1:
+            return 0
+            
+        for i in range(2,int(math.sqrt(A))+1):
+            if A%i == 0:
+                return 0
+                
+        return 1
